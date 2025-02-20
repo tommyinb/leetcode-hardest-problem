28a7bda44743e62eddc7a8331d001e2f0237fc14
Tommy <tommy@tom.my>
2025-02-20 11:45:35 +0800

    use big integer

--- src/solutions/getCovering.ts
+++ src/solutions/getCovering.ts
@@ -2,16 +2,19 @@ import { Question } from "../questions/question";
 
 export function getCovering(question: Question) {
   return (
-    question.circles.find(
-      (circle) =>
-        Math.pow(circle.x, 2) + Math.pow(circle.y, 2) <=
-        Math.pow(circle.radius, 2)
-    ) ??
-    question.circles.find(
-      (circle) =>
-        Math.pow(circle.x - question.area.width, 2) +
-          Math.pow(circle.y - question.area.height, 2) <=
-        Math.pow(circle.radius, 2)
-    )
+    question.circles.find((circle) => {
+      const x = BigInt(circle.x);
+      const y = BigInt(circle.y);
+      const radius = BigInt(circle.radius);
+
+      return x * x + y * y <= radius * radius;
+    }) ??
+    question.circles.find((circle) => {
+      const x = BigInt(circle.x - question.area.width);
+      const y = BigInt(circle.y - question.area.height);
+      const radius = BigInt(circle.radius);
+
+      return x * x + y * y <= radius * radius;
+    })
   );
 }
